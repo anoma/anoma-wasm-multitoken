bb31849ef2a2605a78e2eb78f9f6f8198f175ecc
James Hiew <james@chainfork.com>
2022-06-15 10:01:07 +0100

    tx_mint_multitoken: test deserialization of data

--- crates/tx_mint_multitoken/src/lib.rs
+++ crates/tx_mint_multitoken/src/lib.rs
@@ -22,21 +22,32 @@ fn apply_tx_aux(tx_data: Vec<u8>) -> Result<()> {
 
 #[cfg(test)]
 mod tests {
+    use anoma::proto::Tx;
+    use anoma::types::key::common::SecretKey;
     use anoma_tests::tx::*;
+    use rand::prelude::ThreadRng;
 
     use super::*;
 
-    /// An example test, checking that this transaction performs no storage
-    /// modifications.
+    fn random_key() -> SecretKey {
+        let mut rng: ThreadRng = rand::thread_rng();
+        let sk: SecretKey = {
+            use anoma::types::key::{ed25519, SecretKey, SigScheme};
+            ed25519::SigScheme::generate(&mut rng).try_to_sk().unwrap()
+        };
+        sk
+    }
+
     #[test]
-    fn test_no_op_transaction() {
-        // The environment must be initialized first
+    fn test_deserializing_tx_data() {
         tx_host_env::init();
 
         let tx_data = vec![];
-        apply_tx(tx_data);
+        assert!(apply_tx_aux(tx_data).is_err());
 
-        let env = tx_host_env::take();
-        assert!(env.all_touched_storage_keys().is_empty());
+        let unsigned_data = b"some data".try_to_vec().unwrap();
+        let sk = random_key();
+        let tx_data = Tx::new(vec![], Some(unsigned_data)).sign(&sk).data.unwrap();
+        assert!(apply_tx_aux(tx_data).is_ok());
     }
 }
