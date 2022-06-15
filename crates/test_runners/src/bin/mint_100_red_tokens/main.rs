c68c4d3fbc185981ea352e41367adbf61cf25bf7
James Hiew <james@chainfork.com>
2022-06-15 20:36:30 +0100

    print output of command

--- crates/test_runners/src/bin/mint_100_red_tokens/main.rs
+++ crates/test_runners/src/bin/mint_100_red_tokens/main.rs
@@ -5,5 +5,5 @@ fn main() {
         .args(["--ledger-address", "$ANOMA_LEDGER_ADDRESS", "epoch"])
         .output()
         .expect("failed to execute process");
-    println!("{}", output);
+    println!("{:?}", output);
 }
