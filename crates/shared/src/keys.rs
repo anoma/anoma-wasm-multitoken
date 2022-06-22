1e372c95017bf79fb286d6ec2547f004102081ba
James Hiew <james@chainfork.com>
2022-06-22 15:42:40 +0100

    shared: more keys.rs testing

--- crates/shared/src/keys.rs
+++ crates/shared/src/keys.rs
@@ -29,18 +29,51 @@ pub fn balance(
 #[cfg(test)]
 mod tests {
     use super::*;
+    use anoma::types::storage::DbKeySeg;
+    use anoma_tx_prelude::Address;
 
     const MULTITOKEN_ADDRESS: &str =
         "atest1v4ehgw36g4pyg3j9x3qnjd3cxgmyz3fk8qcrys3hxdp5xwfnx3zyxsj9xgunxsfjg5u5xvzyzrrqtn";
-    const ERC20_ADDRESS: &str = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    const MULTITOKEN_PATH: &str = "ERC20";
+    const DAI_ERC20_ADDRESS: &str = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
     const OWNER_ADDRESS: &str =
         "atest1d9khqw36x9zyxwfhgfpygv2pgc65gse4gy6rjs34gfzr2v69gy6y23zpggurjv2yx5m52sesu6r4y4";
 
     #[test]
-    fn test_balance() {
+    fn test_balance_segment_types() {
+        let key = balance(
+            MULTITOKEN_ADDRESS,
+            MULTITOKEN_PATH,
+            DAI_ERC20_ADDRESS,
+            OWNER_ADDRESS,
+        );
+        assert!(matches!(
+            &key.segments[..],
+            [
+                DbKeySeg::AddressSeg(multitoken_addr),
+                DbKeySeg::StringSeg(multitoken_path),
+                DbKeySeg::StringSeg(token_id),
+                DbKeySeg::StringSeg(balance_key_seg),
+                DbKeySeg::AddressSeg(owner_addr),
+            ] if multitoken_addr == &Address::decode(MULTITOKEN_ADDRESS).unwrap() &&
+            multitoken_path == MULTITOKEN_PATH &&
+            token_id == DAI_ERC20_ADDRESS &&
+            balance_key_seg == BALANCE_KEY_SEGMENT &&
+            owner_addr == &Address::decode(OWNER_ADDRESS).unwrap()
+        ))
+    }
+
+    #[test]
+    fn test_balance_to_string() {
+        let key = balance(
+            MULTITOKEN_ADDRESS,
+            MULTITOKEN_PATH,
+            DAI_ERC20_ADDRESS,
+            OWNER_ADDRESS,
+        );
         assert_eq!(
                 "#atest1v4ehgw36g4pyg3j9x3qnjd3cxgmyz3fk8qcrys3hxdp5xwfnx3zyxsj9xgunxsfjg5u5xvzyzrrqtn/ERC20/0x6B175474E89094C44Da98b954EedeAC495271d0F/balance/#atest1d9khqw36x9zyxwfhgfpygv2pgc65gse4gy6rjs34gfzr2v69gy6y23zpggurjv2yx5m52sesu6r4y4",
-                balance(MULTITOKEN_ADDRESS, "ERC20", ERC20_ADDRESS, OWNER_ADDRESS).to_string()
+                key.to_string()
             )
     }
 }
