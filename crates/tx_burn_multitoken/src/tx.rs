65200ade556d68a4928651d2f352b3f9a87a270d
James Hiew <james@chainfork.com>
2022-06-23 12:39:33 +0100

    tx_burn_multitoken: add integration test

--- crates/tx_burn_multitoken/src/tx.rs
+++ crates/tx_burn_multitoken/src/tx.rs
@@ -47,8 +47,23 @@ fn apply_tx_aux(tx_data: Vec<u8>) -> Result<()> {
 mod tests {
 
     use anoma_tests::tx::*;
+    use anoma_tx_prelude::{address, token::Amount, BorshSerialize, Signed};
+
+    use anoma::proto::Tx;
+    use anoma::types::key::common::SecretKey;
+
+    use rand::prelude::ThreadRng;
 
     use super::*;
+
+    fn random_key() -> SecretKey {
+        let mut rng: ThreadRng = rand::thread_rng();
+        let sk: SecretKey = {
+            use anoma::types::key::{ed25519, SecretKey, SigScheme};
+            ed25519::SigScheme::generate(&mut rng).try_to_sk().unwrap()
+        };
+        sk
+    }
 
     #[test]
     fn test_cannot_pass_empty_data() {
@@ -59,5 +74,53 @@ mod tests {
 
         let env = tx_host_env::take();
         assert!(env.all_touched_storage_keys().is_empty());
+    }
+
+    #[test]
+    fn test_burn_40_of_50_tokens() {
+        let multitoken = address::testing::gen_established_address();
+        let user = address::testing::gen_established_address();
+        let token = address::xan();
+        const MULTITOKEN_PATH: &str = "multitoken";
+        const TOKEN_ID: &str = "blue";
+        let initial = Amount::from(50);
+        let to_burn = Amount::from(40);
+        let inner = multitoken::MultitokenAmount {
+            multitoken_address: multitoken.encode(),
+            multitoken_path: MULTITOKEN_PATH.to_owned(),
+            token_id: TOKEN_ID.to_owned(),
+            owner_address: user.encode(),
+            amount: to_burn,
+        };
+        tx_host_env::init();
+        tx_host_env::with(|tx_env| {
+            tx_env.spawn_accounts(vec![&multitoken, &user, &token]);
+            tx_env
+                .storage
+                .write(&inner.balance_key(), initial.try_to_vec().unwrap())
+                .unwrap();
+            tx_env
+                .storage
+                .write(&inner.supply_key(), initial.try_to_vec().unwrap())
+                .unwrap();
+        });
+        let burn = multitoken::Op::Burn(inner.clone());
+        let inner_sk = random_key();
+        let burn = Signed::<multitoken::Op>::new(&inner_sk, burn);
+        let data = burn.try_to_vec().unwrap();
+        let outer_sk = random_key();
+        let tx_data = Tx::new(vec![], Some(data)).sign(&outer_sk).data.unwrap();
+
+        let result = apply_tx_aux(tx_data);
+
+        if let Err(err) = result {
+            panic!("apply_tx_aux error: {:?}", err);
+        }
+        let env = tx_host_env::take();
+        assert_eq!(env.all_touched_storage_keys().len(), 2);
+        assert!(env
+            .all_touched_storage_keys()
+            .contains(&inner.balance_key()));
+        assert!(env.all_touched_storage_keys().contains(&inner.supply_key()));
     }
 }
